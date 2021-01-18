238992ab07b05bdd4f63450fce6a39410ead73ba
mzulfikaras <zulfikaralisunan96@gmail.com>
2021-01-18 22:47:37 +0700

    kerangka main

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('user')->group( function(){
+
+});
+
+Route::prefix('admin')->group( function(){
+    
+});
