3d0f9efea621aa3c88cc537f27701a9c7d9b68c6
Levarentz <levarentz2001@gmail.com>
2021-02-01 15:35:59 +0700

    merge dengan git main

---
+++ app/Http/Controllers/ContactUsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ContactUs;
+use Illuminate\Http\Request;
+
+class ContactUsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contact = ContactUs::all();
+        return view('admin.contact.show',compact('contact'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $contact = ContactUs::all();
+        return view('admin.contact.create',compact('contact'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama'=>'required',
+            'email'=>'required',
+            'telepon'=>'required',
+            'pesan'=>'required'
+        ])
+        ContactUs::create($validatedData);
+        $request->session()->flash('pesan',"data {$validatedData['nama'] Berhasil Disimpan");
+        return redirect()->view('admin.contact.show')
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ContactUs  $contactUs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ContactUs $contactUs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ContactUs  $contactUs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ContactUs $contactUs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ContactUs  $contactUs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ContactUs $contactUs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ContactUs  $contactUs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ContactUs $contactUs)
+    {
+        //
+    }
+}
