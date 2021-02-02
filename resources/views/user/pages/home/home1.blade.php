c88cb2f2dd9945baa2880a6b74c1ec1e427ab1c0
pmohamadrizkyr rizky <pmohamadrizkyr pmohamadrizkyramadhan@yahoo.com>
2021-02-02 11:03:25 +0700

    membuat tambahan home1

---
+++ resources/views/user/pages/home/home1.blade.php
@@ -0,0 +1,253 @@
+<div class="container-fluid">
+    <div class="badan" style="margin-top: 10px;">
+        <div class="row justify-content-center text-center" >
+            <div class="col-xl-12">
+              <div id="carouselExampleFade" class="carousel-fade" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleFade" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleFade" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleFade" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="img/tokyo3.jpeg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-md-block">
+                      <h3 style="color:aliceblue; font-family: fantasy; font-weight: bold; font-style: italic; font-size: 50px;">Nihon Go</h3>
+                      <button type="button" class="btn btn-primary">Explore Now</button>
+                      <p style="color:whitesmoke; font-family: serif; font-style: italic; font-size: 20px;">Lets Go</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/tokyo2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-md-block">
+                      <h3 style="color:aliceblue; font-family: fantasy; font-weight: bold; font-style: italic; font-size: 50px;">Nihon Go</h3>
+                      <button type="button" class="btn btn-primary">Explore Now</button>
+                      <p style="color:whitesmoke; font-family: serif; font-style: italic; font-size: 20px;">Lets Go</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/tokyo1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-md-block">
+                      <h3 style="color:aliceblue; font-family: fantasy; font-weight: bold; font-style: italic; font-size: 50px;">Nihon Go</h3>
+                      <button type="button" class="btn btn-primary">Explore Now</button>
+                      <p style="color:whitesmoke; font-family: serif; font-style: italic; font-size: 20px;">Lets Go</p>
+                    </div>
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+        </div>
+        <!-- Portofolio -->
+        <div class="portofolio">
+          <div class="row justify-content-center text-center">
+            <div class="col-md-6">
+              <h2 style="color: lightgreen;">Popular Destination</h2>
+            </div>
+          </div>
+          </div>
+        </div>  
+        <div class="container">
+          <div class="row justify-content-center" style="width: 80%; margin-left: 10%;">
+              <div class="col-md-4">
+                  <div class="card">
+                    <button type="button" class="btn btn-light">
+                      <img class="img-fluid" src="img/tmp/gmb2.jpg" class="card-img-top" alt="...">
+                        <p class="card-text" style="color:whitesmoke; font-family: serif; font-style: italic; font-size: 20px;">Lets Go</p>
+                    </button>
+                  </div>
+              </div>
+              <div class="col-md-4">
+                  <div class="card">
+                    <button type="button" class="btn btn-light">
+                      <img class="img-fluid" src="img/tmp/gmb1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                      </div>
+                    </button>
+                  </div>
+              </div>
+              <div class="col-md-4">
+                  <div class="card">
+                    <button type="button" class="btn btn-light">
+                      <img class="img-fluid" src="img/tmp/gb3.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                      </div>
+                    </button>
+                  </div>
+              </div>
+          </div>
+          <div class="row justify-content-center" style="width: 80%; margin-left: 10%;">
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb5.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb6.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+          </div>
+          <div class="row justify-content-center" style="width: 80%; margin-left: 10%;">
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb8.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb9.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+          </div>
+          <div class="row justify-content-center" style="width: 80%; margin-left: 10%;">
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb10.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb11.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                  <button type="button" class="btn btn-light">
+                    <img class="img-fluid" src="img/tmp/gmb12.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Museum Kebangkitan Nasional – 360 View</p>
+                    </div>
+                  </button>
+                </div>
+            </div>
+          </div>  
+        </div>
+        <div class="row justify-content-center">
+          <div class="btn-group content-center mt-5" role="group" aria-label="Basic example">
+              <button type="button" class="btn btn-light">Back</button>
+              <div class="btn-group mr-2 ml-2" role="group" aria-label="First group">
+                  <button type="button" class="btn btn-light">1</button>
+                  <button type="button" class="btn btn-light">2</button>
+                  <button type="button" class="btn btn-light">3</button>
+                  <button type="button" class="btn btn-light">4</button>
+              </div>
+              <button type="button" class="btn btn-light">Next</button>
+          </div>
+        </div>
+        <div class="row justify-content-center" style="margin-top: 50px;">
+          <div class="col-md-6">
+            <div class="embed-responsive embed-responsive-21by9" style="height: 300px;">
+              <iframe width="697" height="392" src="https://www.youtube.com/embed/gD7qFeOXMj0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center" style="margin-left: 10%; margin-right: 10%; margin-top: 50px;">
+          <div class="col-md-4">
+            <div class="card" style="background-color: transparent; border: transparent;">
+              <img style="margin-left: 25%; width: 50%; height: 100px; border-radius: 50%;" src="img/tokyo1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card" style="background-color: transparent; border: transparent;">
+              <img style="margin-left: 25%; width: 50%; height: 100px; border-radius: 50%;" src="img/tokyo2.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card" style="background-color: transparent; border: transparent;">
+              <img style="margin-left: 25%; width: 50%; height: 100px; border-radius: 50%;" src="img/tokyo3.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+<div class="row row-xl justify-content-center text-center" style="margin-top: 50px; background-color:lightskyblue;">
+  <div class="col-md-2">
+    <h6 style="padding-top:15px;">Where You Went To Go?</h6>
+  </div>
+  <div class="col-md-1"></div>
+  <div class="col-md-2" style="padding-top:10px;">
+    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+  </div>
+  <div class="col-md-2" style="padding-top:10px;">
+    <input class="form-control" type="date" id="birthday" name="birthday">
+  </div>
+  <div class="col-md-2" style="padding-top:10px;">
+    <div class="input-group mb-3">
+      <select class="custom-select" id="inputGroupSelect01">
+        <option selected>Choose...</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+  </div>
+</div>
